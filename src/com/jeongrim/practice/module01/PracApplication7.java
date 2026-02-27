96b06d135fb247b0f9217bd8f7838c76ed37e406
2023158013-tech <2023158013@g.eulji.ac.kr>
2026-02-27 09:21:14 +0900

    02.27

--- src/com/jeongrim/practice/module01/PracApplication7.java
+++ src/com/jeongrim/practice/module01/PracApplication7.java
@@ -2,6 +2,17 @@ package com.jeongrim.practice.module01;
 
 public class PracApplication7 {
     public static void main(String[] args) {
+        int numOfApples = 92;
+        int sizeOfBucket = 10;
+        int numOfBucket = 0;
+
+        if (numOfApples % sizeOfBucket >=1 && numOfApples % sizeOfBucket <=10) {
+            numOfBucket = (numOfApples / sizeOfBucket) + 1;
+        } else {
+            numOfBucket = numOfApples / sizeOfBucket;
+        }
+
+        System.out.println("필요한 바구니의 수: " + numOfBucket);
 
     }
 }
